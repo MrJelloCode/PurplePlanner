c6686a3f95c86a4e352c44f7e2fe81ec69729593
Malaravan Vijayakumar <MalaravanV5@gmail.com>
2025-03-21 13:48:34 -0400

    app.java is now functional. Home.fxml was reset and should be ready to start working on actual features

--- src/main/java/com/purple_planner/App.java
+++ src/main/java/com/purple_planner/App.java
@@ -6,33 +6,36 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+
+
 
 /**
  * JavaFX App super duper cool
  */
 public class App extends Application {
 
-    private static Scene scene;
+   
 
+    public static void main(String[] args){
+        launch(args);
+    }
+
+
+    // Stops the warning given when the stage from javaFX is passed. 
+     @SuppressWarnings("exports")
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    public void start(Stage stage) throws Exception {
+
+        
+
+        // Create the canvas so that it loads in home.fxml
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Scene scene = new Scene(root);
+        // Show the user the canvas
         stage.setScene(scene);
         stage.show();
-    }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+    
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
 }
